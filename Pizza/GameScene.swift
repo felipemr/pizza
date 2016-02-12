ac3c5d8128e7159c695df461ded02975f68e1ab9
Felipe Marques Ramos <feliperamos@frenwir.local>
2016-02-12 16:33:51 -0200

    um visu um tanto melhor

--- Pizza/GameScene.swift
+++ Pizza/GameScene.swift
@@ -13,7 +13,10 @@ class GameScene: SKScene {
     
     var myFloor1 = SKSpriteNode()
     var myFloor2 = SKSpriteNode()
-    var player = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 50, height: 50))
+//    var player1 = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 50, height: 50))
+    
+    var player = SKSpriteNode(imageNamed: "Spaceship")
+    var fatMan = SKSpriteNode(imageNamed: "fatMan")
     override func didMoveToView(view: SKView) {
 
         let swipeRight:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: Selector("swiped:"))
@@ -25,32 +28,53 @@ class GameScene: SKScene {
         swipeLeft.direction = .Left
         view.addGestureRecognizer(swipeLeft)
 
-        myFloor1 = SKSpriteNode(imageNamed: "fokd")
-        myFloor1.color = UIColor.blueColor()
-        myFloor1.colorBlendFactor = 1
-        myFloor1.position = CGPointMake( self.size.width/2, 0);
+        setUpBg()
         
-        myFloor2 = SKSpriteNode(imageNamed: "fokd")
-        myFloor2.colorBlendFactor = 1
-        myFloor2.color = UIColor.brownColor()
-        myFloor2.position = CGPointMake(self.size.width/2, myFloor1.size.height-1);
+        setUpPlayer()
         
+//        setUpEnemy()
+
+    }
+    
+    func setUpBg(){
+        myFloor1 = SKSpriteNode(imageNamed: "back")
+        myFloor1.position = CGPointMake( self.size.width/2, 0)
         
-        player.fillColor = UIColor.yellowColor()
-        player.position.x = self.view!.center.x - player.frame.width/2
-        player.position.y = self.view!.center.y - 200
-        player.zPosition = 1000
+        myFloor2 = SKSpriteNode(imageNamed: "back")
+        myFloor2.position = CGPointMake(self.size.width/2, myFloor1.size.height-1)
+        
         
         self.addChild(myFloor1)
         self.addChild(myFloor2)
+    }
+    func setUpPlayer(){
+        player.anchorPoint = CGPointZero
+        player.size = CGSize(width: 50, height: 50)
+        player.position.x = self.view!.center.x - player.frame.width/2
+        player.position.y = self.view!.center.y - 200
+        player.zPosition = 1000
         self.addChild(player)
+    }
+    func setUpEnemy(){
+        fatMan.position = CGPoint(x: self.size.width/2 + 35, y: 3*(self.size.height/4))
+        fatMan.size = CGSize(width: 50, height: 50)
+        fatMan.zPosition = 90
+        self.addChild(fatMan)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
     }
     var speedy: CGFloat = 10
+    var lastTime = 0.0
+    var timer = 0.0
+    var firstTime = true
+    
+    
+    
     override func update(currentTime: CFTimeInterval) {
+        
+        
         /* Called before each frame is rendered */
         myFloor1.position = CGPointMake(myFloor1.position.x, myFloor1.position.y-speedy);
         
@@ -67,18 +91,34 @@ class GameScene: SKScene {
             myFloor2.position = CGPointMake(myFloor1.position.x, myFloor1.position.y + myFloor2.size.height);
             
         }
-
+        
+        if firstTime {
+            firstTime = false
+            lastTime = currentTime
+        }else{
+            let delta = currentTime - lastTime
+            lastTime = currentTime
+        
+            if timer > 10 {
+                speedy++
+                timer = 0
+            }
+            else{
+                timer += delta
+            }
+        }
+        
     }
     
     func swiped(sender:UISwipeGestureRecognizer){
         if sender.direction == .Left{
             print("left")
-            player.position.x -= 10
+            player.position.x -= speedy
         }
         
         if sender.direction == .Right{
             print("right")
-            player.position.x += 10
+            player.position.x += speedy
         }
     }
 }
